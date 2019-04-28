65e7734717b95dba5176db0a01a916c0d71af5f7
MagnusCardell <cardell.magnus@gmail.com>
2019-04-28 18:05:58 +0200

    updated content

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "MagnusCardell", selector: "#ghfeed" });
-
-
-});
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
